5addf95c95e51c70f4b828ae38dc1dd617afa343
unknown <gopala20021810@gmail.com>
2024-04-24 11:17:06 +0530

    madhu

--- DynamicAmbitionbox.js
+++ DynamicAmbitionbox.js
@@ -2,57 +2,63 @@ import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
 import xlsx from 'xlsx'
 import fs from 'fs'
+import express from 'express';
+
+
 
 puppeteer.use(StealthPlugin());
 
-(async () => {
+const app = express();
+
+async function scrapeData(){
+
   try {
     const companyName = 'Quess'
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-
+  
     const waitForSelector = async (selector) => {
       await page.waitForSelector(selector);
       console.log("Element is loading");
     };
-
+  
     // Function to scrape overall ratings
     const scrapeOverallRatings = async (companyName) => {
       const url = `https://www.ambitionbox.com/reviews/${companyName}-reviews`;
       await page.goto(url);
       console.log("website loaded")
-
+  
       await waitForSelector("#ab_company_review_rating_card > div.ab_section-header.container.header.flex.header-end > div > h1");
-
+  
       const CompanyName = await page.$eval("#ab_company_review_rating_card > div.ab_section-header.container.header.flex.header-end > div > h1", h1 => h1.textContent.trim());
       console.log("Company Name:", CompanyName);
-
+  
       const overallrating_data = [];
-
+  
       await page.waitForSelector("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.badge-cont > div.badge-x-large.rating-4 > span");
       const Overall_Rating = await page.$eval("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.badge-cont > div.badge-x-large.rating-4 > span", span => span.textContent.trim());
       console.log(Overall_Rating);
-
+  
       await page.waitForSelector("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(1) > label > p.stars_values.count.body-medium");
       const FiveStar_Rating = await page.$eval("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(1) > label > p.stars_values.count.body-medium", p => p.textContent.trim());
       console.log(FiveStar_Rating);
-
+  
       await page.waitForSelector("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(2) > label > p.stars_values.count.body-medium");
       const FourStar_Rating = await page.$eval("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(2) > label > p.stars_values.count.body-medium", p => p.textContent.trim());
       console.log("Fourstar:", FourStar_Rating);
-
+  
       await page.waitForSelector("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(3) > label > p.stars_values.count.body-medium");
       const ThreeStar_Rating = await page.$eval("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(3) > label > p.stars_values.count.body-medium", p => p.textContent.trim());
       console.log(ThreeStar_Rating);
-
+  
       await page.waitForSelector("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(4) > label > p.stars_values.count.body-medium");
       const TwoStar_Rating = await page.$eval("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(4) > label > p.stars_values.count.body-medium", p => p.textContent.trim());
       console.log(TwoStar_Rating);
-
+  
       await page.waitForSelector("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(5) > label > p.stars_values.count.body-medium");
       const OneStar_Rating = await page.$eval("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.rating_stats_bars > div:nth-child(5) > label > p.stars_values.count.body-medium", p => p.textContent.trim());
       console.log("onestar",OneStar_Rating);
-
+  
       overallrating_data.push({
         'Company Name': CompanyName,
         'Overall Rating': Overall_Rating,
@@ -62,17 +68,17 @@ puppeteer.use(StealthPlugin());
         'Two Star Rating': TwoStar_Rating,
         'One Star Rating': OneStar_Rating
       });
-
+  
       return overallrating_data;
     };
-
+  
     // Function to scrape employee reviews
     const scrapeEmployeeReviews = async (companyName) => {
       const url = `https://www.ambitionbox.com/reviews/${companyName}-reviews`;
       await page.goto(url);
       
       await waitForSelector(".reviewer-info > h2");
-
+  
       const Employee_roles_and_locations = await page.$$eval(".reviewer-info > h2", h2s => {
         return h2s.map(h2 => {
           const text = h2.textContent.trim();
@@ -87,28 +93,28 @@ puppeteer.use(StealthPlugin());
           return null;
         }).filter(data => data !== null);
       });
-
+  
       const Job_Types = await page.$$eval(".reviewer-info > div > div > p", paragraphs => {
         return paragraphs.map(p => p.textContent.trim());
       });
-
+  
       const Employee_Department = await page.$$eval(".user-wrap > p", paragraphs => {
         return paragraphs.map(p => p.textContent.trim());
       });
-
+  
       const Posted_Dates = await page.$$eval(".badgePstdWrpr > span", spans => {
         return spans.map(span => {
           const match = span.textContent.trim().match(/posted on (\d{2} [a-zA-Z]+ \d{4})/);
           return match ? match[1] : "";
         });
       });
-
+  
       const Employee_Ratings = await page.$$eval(".badgePstdWrpr > div > span > span", spans => {
         return spans.map(span => span.textContent.trim());
       });
-
+  
       const employeeReviews = await page.$$("div.review-content-wrap > div.review-content-cont > div");
-
+  
       const employeeData = [];
       for (const review of employeeReviews) {
         const Emp_likes = await review.$eval("div:nth-child(1) > p", p => p.textContent.trim());
@@ -119,7 +125,7 @@ puppeteer.use(StealthPlugin());
         };
         employeeData.push(rowData);
       }
-
+  
       const data = [];
       for (let i = 0; i < Employee_roles_and_locations.length; i++) {
         data.push({
@@ -133,75 +139,99 @@ puppeteer.use(StealthPlugin());
           'Dislikes': employeeData[i].dislikes,
         });
       }
-
+  
       return data;
     };
-
+  
     // Function to scrape year trend ratings
     const scrapeYearTrendRatings = async (companyName) => {
       const url = `https://www.ambitionbox.com/reviews/${companyName}-reviews`;
       await page.goto(url);
-
+  
       await waitForSelector("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.badge-cont > div.caption-strong-large.toggle-trend-cta > span");
       const elementSelector = await page.$("#ab_company_review_rating_card > div.card.globalCard.globalCard--elevated > div > div.overall-wrap > div > div.badge-cont > div.caption-strong-large.toggle-trend-cta > span");
       await elementSelector.click();
       console.log("Rating trend is clicked");
-
+  
       await page.evaluate(() => {
         return new Promise(resolve => {
             setTimeout(resolve, 2000); // Wait for 2000 milliseconds (2 seconds)
         });
       });
-
+  
       const yearData = await page.evaluate(() => {
         const yearSelectors = document.querySelectorAll("#LCgraphContainer > div > div.bold-list-header.year");
         const ratingSelectors = document.querySelectorAll("#LCgraphContainer > div > div.label");
         const descriptionSelectors = document.querySelectorAll("#LCgraphContainer > div > div.line-container > div.circle-pointer > div > span");
-
+  
         const yearData = [];
-
+  
         // Iterate over each selector to gather data
         yearSelectors.forEach((yearSelector, index) => {
           const year = yearSelector.textContent.trim();
           const rating = ratingSelectors[index].textContent.trim();
           const description = descriptionSelectors[index].textContent.trim();
-
+  
           yearData.push({ year, rating, description });
         });
-
+  
         return yearData;
       });
       return yearData;
     };
-
+  
     // Scrape data from all sources
     const overallRatingsData = await scrapeOverallRatings(companyName);
     const employeeReviewsData = await scrapeEmployeeReviews(companyName);
     const yearTrendRatingsData = await scrapeYearTrendRatings(companyName);
-
+  
     // Write data to Excel
     const wb = xlsx.utils.book_new();
-
+  
     const overallRatingsSheet = xlsx.utils.json_to_sheet(overallRatingsData);
     xlsx.utils.book_append_sheet(wb, overallRatingsSheet, 'Overall Ratings');
-
+  
     const employeeReviewsSheet = xlsx.utils.json_to_sheet(employeeReviewsData);
     xlsx.utils.book_append_sheet(wb, employeeReviewsSheet, 'Employee Reviews');
-
+  
     const yearTrendRatingsSheet = xlsx.utils.json_to_sheet(yearTrendRatingsData);
     xlsx.utils.book_append_sheet(wb, yearTrendRatingsSheet, 'Year Trend Ratings');
-
+  
     const excelData = xlsx.write(wb, { bookType: 'xlsx', type: 'buffer' });
-
+  
     fs.writeFileSync('AmbitionBox-overalldata.xlsx', excelData);
-
+  
     console.log("Excel file created successfully!");
-
+  
     // Close the browser
     await browser.close();
-
+  
     
   } catch (error) {
     console.error("An error occurred:", error);
   }
-})();
+}
+
+
+
+app.get('/download-excel', async (req, res) => {
+  const companyName = req.query.companyName;
+
+
+  try {
+      // Call the scrapeData function
+      const filePath = await scrapeData();
+      
+      // Send the Excel file as a response
+      res.download(filePath);
+  } catch (error) {
+      console.error('Error scraping data:', error);
+      res.status(500).send('Internal Server Error');
+  }
+});
+
+// Start the server on a specific port
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
